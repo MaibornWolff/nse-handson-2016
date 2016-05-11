775c6830de5f3de6d1e23dd65dd95e507550e659
Philipp Lamp <philipp.lamp@maibornwolff.de>
2016-05-11 14:44:30 +0200

    autostart

--- nse_autostart.sh
+++ nse_autostart.sh
@@ -1,3 +1,3 @@
 export HANDSON_SERVER=mweanse.ddns.net
-cd handson/badge-reader/build/libs
+cd nse-handson-2016/badge-reader/build/libs
 java -jar badge-reader.jar --consulHost=mweanse.ddns.net --deviceId=$1
